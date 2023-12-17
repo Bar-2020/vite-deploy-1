4f49de45ec21b992516d6787b16219c3fd8a95cf
Bar Almog <baralmog@protonmail.com>
2023-12-17 15:56:44 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,16 @@ function App() {
 
   return (
     <>
-      <img src={ofirImg} alt="ofir img" />
+      <img src={ofirImg} alt="Ofir img" style={{ borderRadius: 15 }} />
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Deployment here!</h1>
+      <h1>Deployment here! First push</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
